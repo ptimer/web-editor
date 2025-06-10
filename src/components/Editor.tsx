e46601a6c32ea42e5195b32ce585375507428ac8
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 00:54:14 +0300

    feat: editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,14 @@
+import { MockBlocks } from "@/common/constants";
+import { EditableBlock } from "@/components";
 export const Editor = () => {
   return (
-    <div>
-        EDITOR
+    <div className="bg-grey-lightest px-30 py-20 w-538 flex flex-col gap-15">
+        {MockBlocks.map(block => (
+          <EditableBlock
+            key={block.type}
+            data={block}
+          />
+        ))}
     </div>
   )
 }
