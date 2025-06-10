b6a1f13acabd6b61490c35e6146c6bb1e2fcd1ce
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 01:39:52 +0300

    feat: no support for mobiles message

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex flex-1">
+      <main className="hidden flex-1 lg:flex">
         <Sidebar />
         <Editor />
         <Preview />
       </main>
+      <div className="flex flex-1 items-center justify-center lg:hidden">
+        <h1 className="text-custom-heading-01 text-center">
+            Sorry, but there is no mobile version of this app yet.
+            <br />
+            Please use your desktop browser to view this app.
+        </h1>
+      </div>
     </div>
   )
 }
