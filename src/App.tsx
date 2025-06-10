23c5916c5ca91a9f0f6afd3a7bcf6089fe8fc1be
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 00:54:20 +0300

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
-import { Header } from '@/components'
+import { Header, Sidebar, Editor, Preview } from '@/components'
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main>
-        
+      <main className="flex flex-1">
+        <Sidebar />
+        <Editor />
+        <Preview />
       </main>
-    </>
+    </div>
   )
 }
 
