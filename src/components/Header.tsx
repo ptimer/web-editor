f81a41f8312a9975aa505e41eb234de6d4a5d6ba
Illia Bilous <bilous.job@yahoo.com>
2025-06-10 14:45:10 +0300

    feat: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="pt-21 pb-22 px-31 border-b-1 border-[#E4E6F1]">
+        <h1 className="text-custom-heading-02">REACT EDITOR Test</h1>
+    </header>
+  )
+}
