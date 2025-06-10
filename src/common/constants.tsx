9048f0df5b0b8a3a174c38067e4e13daed8b19f8
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 02:50:17 +0300

    refactor: mock blocks

--- src/common/constants.tsx
+++ src/common/constants.tsx
@@ -1,11 +1,10 @@
-import { HeadlineIcon, ImageIcon, TextAlignLeftIcon } from "@/components";
-
-export const MockBlocks: BlockData[] = [
-  { id: '1', order: 2, type: 'headline', label: 'Headline', icon: <HeadlineIcon />, content: 'A legendary cap set that feels like new' },
-  { id: '2', order: 3, type: 'paragraph', label: 'Paragraph', icon: <TextAlignLeftIcon />, content: `
+export const mockBlocks: BlockData[] = [
+  { id: '1', order: 2, type: 'headline', content: 'A legendary cap set that feels like new' },
+  { id: '2', order: 3, type: 'paragraph', content: `
     Prompt your customer to revisit your store by showing them the products they left behind. Consider offering them a coupon code to close the deal.
-Using the "Insert" panel on the right, drag and drop 
-the Abandoned Cart element onto the page below.` },
-  { id: '3', order: 4, type: 'button', label: 'Button', icon: <ImageIcon />, text: 'Register now' },
-  { id: '4', order: 1, type: 'image', label: 'Image', icon: <ImageIcon />, alt: 'Image', url: 'https://media.istockphoto.com/id/1399246824/uk/%D1%84%D0%BE%D1%82%D0%BE/%D1%86%D0%B8%D1%84%D1%80%D0%BE%D0%B2%D1%96-%D0%BB%D1%96%D0%BD%D1%96%D1%97-%D1%85%D0%B2%D0%B8%D0%BB%D1%8C-%D0%BE%D1%87%D0%B5%D0%B9-%D1%81%D1%82%D0%BE%D0%BA%D0%BE%D0%B2%D0%B8%D0%B9-%D1%84%D0%BE%D0%BD.jpg?s=2048x2048&w=is&k=20&c=iXYM50PebeJ5hBfhKleK_xL-gDDMbMVxNG1C6VO4984=' },
+    Using the "Insert" panel on the right, drag and drop 
+    the Abandoned Cart element onto the page below.`
+  },
+  { id: '3', order: 4, type: 'button', text: 'Register now' },
+  { id: '4', order: 1, type: 'image', alt: 'Image', url: 'https://media.istockphoto.com/id/1399246824/uk/%D1%84%D0%BE%D1%82%D0%BE/%D1%86%D0%B8%D1%84%D1%80%D0%BE%D0%B2%D1%96-%D0%BB%D1%96%D0%BD%D1%96%D1%97-%D1%85%D0%B2%D0%B8%D0%BB%D1%8C-%D0%BE%D1%87%D0%B5%D0%B9-%D1%81%D1%82%D0%BE%D0%BA%D0%BE%D0%B2%D0%B8%D0%B9-%D1%84%D0%BE%D0%BD.jpg?s=2048x2048&w=is&k=20&c=iXYM50PebeJ5hBfhKleK_xL-gDDMbMVxNG1C6VO4984=' },
 ]
