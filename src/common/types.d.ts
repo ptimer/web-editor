bf12d7ea6b88cfb00337282c5b984d282a0ac46a
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 00:51:19 +0300

    feat: types

--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -1,7 +1,35 @@
-type ElementType = 'headline' | 'paragraph' | 'image' | 'button';
+type BlockType = 'headline' | 'paragraph' | 'image' | 'button';
 
-interface ElementProps {
-    type: ElementType;
+interface BaseBlock {
+    id: string;
+    type: BlockType;
     label: string;
-    icon: React.ReactNode;
+    icon: JSX.Element;
+    order: number;
 }
+
+interface HeadlineBlock extends BaseBlock {
+    type: 'headline';
+    content: string;
+}
+
+interface ParagraphBlock extends BaseBlock {
+    type: 'paragraph';
+    content: string;
+}
+
+interface ImageBlock extends BaseBlock {
+    type: 'image';
+    url: string;
+}
+
+interface ButtonBlock extends BaseBlock {
+    type: 'button';
+    text: string;
+}
+
+type BlockData =
+    | HeadlineBlock
+    | ParagraphBlock
+    | ImageBlock
+    | ButtonBlock;
