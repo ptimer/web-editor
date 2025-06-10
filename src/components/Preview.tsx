1058677ab4deaab915abd5093abda1772f0a1f4e
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 01:39:24 +0300

    feat: preview

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,7 +1,31 @@
+import { MockBlocks } from "@/common/constants"
+import { Button } from "@/components"
+
+const sortByOrder = (a: BlockData, b: BlockData) => a.order - b.order;
+
 export const Preview = () => {
+  const renderBlock = (block: BlockData) => {
+    switch(block.type) {
+      case 'headline':
+        return <h1 className="text-custom-heading-01 text-dark-grey">{block.content}</h1>
+      case 'paragraph':
+        return <p className="text-custom-body-01 text-blue-grey text-center">{block.content}</p>
+      case 'button':
+        return <Button>{block.text}</Button>
+      case 'image':
+        return (
+          <div className="w-540">
+            <img src={block.url} alt={block.alt} className="h-auto w-full rounded-sm" />
+          </div>
+        )
+      default:
+        return null
+    }
+  }
+
   return (
-    <div>
-        Preview
+    <div className="flex-1 flex flex-col p-30 gap-30 items-center">
+        {MockBlocks.sort(sortByOrder).map(block => renderBlock(block))}
     </div>
   )
 }
