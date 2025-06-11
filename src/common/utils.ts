98d835123c06c53bbb44c7a1c7e3fab8cecdfab1
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 13:48:16 +0300

    feat: sort blocks by order util

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const sortBlocksByOrder = (blocks: BlockData[]) => blocks.sort((a, b) => a.order - b.order);
