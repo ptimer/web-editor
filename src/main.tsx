3dfdced1d32216053a540df95bd7ae2581aaa18a
Illia Bilous <bilous.job@yahoo.com>
2025-06-11 17:33:34 +0300

    chore: dnd

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import store from '@/store';
 import './globals.css'
 import App from './App.tsx'
@@ -10,10 +13,13 @@ const container = document.getElementById('root')!;
 if (container) {
   const root = createRoot(container);
 
+  // TODO: provider component
   root.render(
     <Provider store={store}>
       <StrictMode>
-        <App />
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
       </StrictMode>
     </Provider>,
   )
